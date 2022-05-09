c0ee4e79fa6bb548ff23aed172db6e18fc99d537
Onur Saltas <60262668+onursaltas@users.noreply.github.com>
2022-05-09 22:52:29 +0300

    Update ProductTest

--- src/test/java/Main/ProductTest.java
+++ src/test/java/Main/ProductTest.java
@@ -1,66 +1,73 @@
 package test.java.Main;
 
+import main.java.base.functions.BrowserController;
 import main.java.page.pages.MainPages.ProductPage;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import test.java.Base.BaseTest;
 
-public class ProductTest extends ProductPage   {
+import java.util.WeakHashMap;
+
+public class ProductTest extends BaseTest implements BrowserController {
+
+
+    ProductPage productPage = new ProductPage();
 
     @Test
     public void T01_SelectAttributeAndAssertThem(){
 
-        SendSearchBox("Bilgisayar");
-        ClickButton();
-        ClickSelectNotebook();
-        ClickselectBrand1();
-        ClickselectBrand2();
-        ClickselectBrand3();
-        ClickSelectSeller();
-        ClickSelectType();
-        ClickSelectResulation();
-        ClickSelectPanelType();
-        ClickSelectCpuFast();
-        ClickSelectMemory1();
-        ClickSelectMemory2();
-        ClickSelectMemory3();
-        ClickSelectScreenSize();
-        ClickSelectScreenResulation();
-        ClickSelectDisplayCard();
-        ClickSelectDisplayCardType();
-        ClickSelectCpuModel();
-        ClickSelectOperationSystem();
-        ClickSelectHardDisk();
-        ClickSelectSsd();
-        ClickSelectPrice();
+        productPage.SendSearchBox("Bilgisayar");
+        productPage.ClickButton();
+        productPage.ClickSelectNotebook();
+        productPage.ClickselectBrand1();
+        productPage.ClickselectBrand2();
+        productPage.ClickselectBrand3();
+        productPage.ClickSelectSeller();
+        productPage.ClickSelectType();
+        productPage.ClickSelectResulation();
+        productPage.ClickSelectPanelType();
+        productPage.ClickSelectCpuFast();
+        productPage.ClickSelectMemory1();
+        productPage.ClickSelectMemory2();
+        productPage.ClickSelectMemory3();
+        productPage.ClickSelectScreenSize();
+        productPage.ClickSelectScreenResulation();
+        productPage.ClickSelectDisplayCard();
+        productPage.ClickSelectDisplayCardType();
+        productPage.ClickSelectCpuModel();
+        productPage.ClickSelectOperationSystem();
+        productPage.ClickSelectHardDisk();
+        productPage.ClickSelectSsd();
+        productPage.ClickSelectPrice();
 
-        String cpuFast = GetCpuFast();
+        String cpuFast = productPage.GetCpuFast();
         Assert.assertEquals("2.6 GHz",cpuFast);
-        String cpuModel = GetCpuModel();
+        String cpuModel = productPage.GetCpuModel();
         Assert.assertEquals("3250U",cpuModel);
-        String displayCardModel = GetDisplayCardModel();
+        String displayCardModel = productPage.GetDisplayCardModel();
         Assert.assertEquals("Radeon Graphics",displayCardModel);
-        String cpuType = GetCpuType();
+        String cpuType = productPage.GetCpuType();
         Assert.assertEquals("AMD Ryzen 3",cpuType);
-        String panelType = GetPanelType();
+        String panelType = productPage.GetPanelType();
         Assert.assertEquals("TN",panelType);
-        String displayCardType = GetDisplayCardType();
+        String displayCardType = productPage.GetDisplayCardType();
         Assert.assertEquals("Paylaşımlı",displayCardType);
-        String hardDisk = GetHardDisk();
+        String hardDisk = productPage.GetHardDisk();
         Assert.assertEquals("1 TB",hardDisk);
-        String memory = GetMemory();
+        String memory = productPage.GetMemory();
         Assert.assertEquals("8 GB",memory);
-        String ssd = GetSSD();
+        String ssd = productPage.GetSSD();
         Assert.assertEquals("256 GB" ,ssd);
-        String operationSystem = GetOperationSystem();
+        String operationSystem = productPage.GetOperationSystem();
         Assert.assertEquals("Win 10 Home" ,operationSystem);
-        String screenResulation = GetScreenResulation();
+        String screenResulation = productPage.GetScreenResulation();
         Assert.assertEquals("1920x1080 px" ,screenResulation);
-        String secreenSize = GetScreenSize();
+        String secreenSize = productPage.GetScreenSize();
         Assert.assertEquals("15.6 inç" ,secreenSize);
-        String resulation = GetResulation();
+        String resulation = productPage.GetResulation();
         Assert.assertEquals("Full HD" ,resulation);
-        String type = GetType();
+        String type = productPage.GetType();
         Assert.assertEquals("Laptop / Notebook" ,type);
 
 
